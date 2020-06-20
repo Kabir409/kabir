8f26a992e229f28368944708fa382188c853df93
Kabir409 <67181563+Kabir409@users.noreply.github.com>
2020-06-20 12:34:30 +0600

    Update SmartBoy.py

--- SmartBoy.py
+++ SmartBoy.py
@@ -48,17 +48,20 @@ def jalan(z):
 
 ##### LOGO #####
 logo = """
-                       
                      
-▒█▀▀▀█ ▒█▀▄▀█ ░█▀▀█ ▒█▀▀█ ▀▀█▀▀ 
-░▀▀▀▄▄ ▒█▒█▒█ ▒█▄▄█ ▒█▄▄▀ ░▒█░░ 
-▒█▄▄▄█ ▒█░░▒█ ▒█░▒█ ▒█░▒█ ░▒█░░
+▒█░▄▀ ░█▀▀█ ▒█▀▀█ ▀█▀ ▒█▀▀█ 
+▒█▀▄░ ▒█▄▄█ ▒█▀▀▄ ▒█░ ▒█▄▄▀ 
+▒█░▒█ ▒█░▒█ ▒█▄▄█ ▄█▄ ▒█░▒█  
+                 
+
+
+
        facebook:Easin.Official10
 \033[1;91m=======================================
 \033[1;96mAuthor  \033[1;93m: \033[1;98mSmart Boy Easin
 \033[1;96mInstagram \033[1;93m: \033[1: md_easin
 \033[1;96mFacebook  \033[1;93m: \033[1: Easin.Official0
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/SmartBoyEasin/SmartBoy
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Kabir409/Kabir
 \033[1;91m======================================="""
 
 def tik():
@@ -81,20 +84,27 @@ print "\033[1;96m ============================================================="
 print  """\033[1;91m
  
             
-▒█▀▀▀█ ▒█▀▄▀█ ░█▀▀█ ▒█▀▀█ ▀▀█▀▀ 
-░▀▀▀▄▄ ▒█▒█▒█ ▒█▄▄█ ▒█▄▄▀ ░▒█░░ 
-▒█▄▄▄█ ▒█░░▒█ ▒█░▒█ ▒█░▒█ ░▒█░░
+▒█░▄▀ ░█▀▀█ ▒█▀▀█ ▀█▀ ▒█▀▀█ 
+▒█▀▄░ ▒█▄▄█ ▒█▀▀▄ ▒█░ ▒█▄▄▀ 
+▒█░▒█ ▒█░▒█ ▒█▄▄█ ▄█▄ ▒█░▒█
+
+
+
        Facebook:Smart Boy Easin
  
 \033[1;96mAuthor  \033[1;93m: \033[1;92mSmart Boy Easin
 \033[1;96mInstagram \033[1;93m: \033[1;92mmd_easin
 \033[1;96mFacebook  \033[1;93m: \033[1;92m Easin.Official0
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/SmartBoyEasin/SmartBoy
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Kabir409/Kabir
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "easin"
-CorrectPassword = "easin"
+CorrectUsername = "Kabir"
+CorrectPassword = "Kabir"
+
+
+
+"
 
 loop = 'true'
 while (loop == 'true'):
